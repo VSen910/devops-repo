698fed01db0da88c89d50f33c2fbc7cbbe9e0c6a
nischaykondai27 <nischaykondai27@gmail.com>
2024-09-20 23:47:37 +0530

    added selection sorth python code

--- example.py
+++ example.py
@@ -77,8 +77,28 @@ def merge_sort(arr, left, right):
         merge(arr, left, mid, right)
 
 #Contributer Nischay Kondai
-def modu(x, y):
-    return x%y
+
+# Selection sort in Python
+# time complexity O(n*n)
+#sorting by finding min_index
+def selectionSort(array, size):
+	
+	for ind in range(size):
+		min_index = ind
+
+		for j in range(ind + 1, size):
+			# select the minimum element in every iteration
+			if array[j] < array[min_index]:
+				min_index = j
+		# swapping the elements to sort the array
+		(array[ind], array[min_index]) = (array[min_index], array[ind])
+
+arr = [-2, 45, 0, 11, -9,88,-97,-202,747]
+size = len(arr)
+selectionSort(arr, size)
+print('The array after sorting in Ascending Order by selection sort is:')
+print(arr)
+
 
 #Contributer Pratik Raj
 def sub(a, b):
